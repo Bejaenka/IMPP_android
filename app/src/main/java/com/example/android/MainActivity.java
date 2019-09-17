f12cdc5fdd370fe19a5046ac54f850c4c1ca66d6
Bejaenka <53152601+Bejaenka@users.noreply.github.com>
2019-09-17 19:53:07 +0200

    Repariert ohne IMPP

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.util.Log;
+//import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private static native String hello(final String to);
+    //private static native String loadData(final String to, final String st);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         System.loadLibrary("rust");
 
-
-        Log.d("rust", this.hello("IMPP"));
+        //Log.d("rust", this.hello("IMPP"));
 
         String r = this.hello("Martin and Bianca");
+
+        //String r = this.loadData("yes", "https://docs.google.com/spreadsheets/d/14fNP2Elca82rryRJ8-a_XwH3_oZgrJyXqh7r7Q7GuEc/edit#gid=0");
         ((TextView)findViewById(R.id.hello)).setText(r);
+
     }
 
 
